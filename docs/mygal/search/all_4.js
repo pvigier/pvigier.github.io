8c55c95312a3767a77394de853a95701400d06bd
Pierre Vigier <pierre.vigier@ymail.com>
2019-05-05 20:54:14 +0200

    Update the website

--- docs/mygal/search/all_4.js
+++ docs/mygal/search/all_4.js
@@ -3,13 +3,15 @@ var searchData=
   ['getdet',['getDet',['../classmygal_1_1Vector2.html#af7f85e0ea606b18c41051832e158ce3e',1,'mygal::Vector2']]],
   ['getdiagram',['getDiagram',['../classmygal_1_1FortuneAlgorithm.html#a8fb0dd45fd087694680e1e2bd7e1f680',1,'mygal::FortuneAlgorithm']]],
   ['getdistance',['getDistance',['../classmygal_1_1Vector2.html#a8d835026720722bd7b58c06765af073b',1,'mygal::Vector2']]],
-  ['getface',['getFace',['../classmygal_1_1Diagram.html#aa067783cb0851e1528904a222a96bf38',1,'mygal::Diagram']]],
+  ['getface',['getFace',['../classmygal_1_1Diagram.html#a54fdef96fcbb93a29431bd48bcdfa69e',1,'mygal::Diagram']]],
+  ['getfaces',['getFaces',['../classmygal_1_1Diagram.html#a4a674d87c490103e2c08615f377b10e3',1,'mygal::Diagram']]],
   ['gethalfedges',['getHalfEdges',['../classmygal_1_1Diagram.html#adfe056f9f0741441e9ba1c2508fad335',1,'mygal::Diagram']]],
   ['getnbsites',['getNbSites',['../classmygal_1_1Diagram.html#ae908919bd45de188edd494a01656bb73',1,'mygal::Diagram']]],
   ['getnbvertices',['getNbVertices',['../classmygal_1_1Triangulation.html#a4393969007156bbde1d3c5f503bbbe1d',1,'mygal::Triangulation']]],
   ['getneighbors',['getNeighbors',['../classmygal_1_1Triangulation.html#afa14f743c8a8469f5225ecadc233817f',1,'mygal::Triangulation']]],
   ['getnorm',['getNorm',['../classmygal_1_1Vector2.html#a8ebc5813d29b67b9e947d68a53baba3a',1,'mygal::Vector2']]],
   ['getorthogonal',['getOrthogonal',['../classmygal_1_1Vector2.html#a77cf9d32f04f1d84f33dabae5b614ac8',1,'mygal::Vector2']]],
-  ['getsite',['getSite',['../classmygal_1_1Diagram.html#a96d1ecfc735a8796c1d9e58754a21bff',1,'mygal::Diagram']]],
+  ['getsite',['getSite',['../classmygal_1_1Diagram.html#ab9c11f36ef56034efb6e62cf0070be5c',1,'mygal::Diagram']]],
+  ['getsites',['getSites',['../classmygal_1_1Diagram.html#aa931c46fb07ef6b8547af77e47dd8ac8',1,'mygal::Diagram']]],
   ['getvertices',['getVertices',['../classmygal_1_1Diagram.html#a126f133623de334fcd7614e2a780bde2',1,'mygal::Diagram']]]
 ];
--- docs/mygal/search/classes_7.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['vector2',['Vector2',['../classmygal_1_1Vector2.html',1,'mygal']]],
-  ['vertex',['Vertex',['../structmygal_1_1Diagram_1_1Vertex.html',1,'mygal::Diagram']]]
-];
